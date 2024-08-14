980e4cc893462be3b0877dbbace0ec959f8ccde9
lhchavez <lhchavez@lhchavez.com>
2024-08-14 20:54:42 +0000

    Better fire-and-forget WS closure
    This introduces more problems than what it solves, since now if the
    application wants to exit, it may orphan this and cause a lot of
    logspam.
    
    This change now closes the websocket in the background task manager so
    that it is not orphaned upon process exit.

--- replit_river/websocket_wrapper.py
+++ replit_river/websocket_wrapper.py
@@ -4,6 +4,8 @@ import logging
 
 from websockets import WebSocketCommonProtocol
 
+from replit_river.task_manager import BackgroundTaskManager
+
 logger = logging.getLogger(__name__)
 
 
@@ -24,12 +26,22 @@ class WebsocketWrapper:
         async with self.ws_lock:
             return self.ws_state == WsState.OPEN
 
-    async def close(self) -> None:
+    async def close(self, tm: BackgroundTaskManager) -> None:
         async with self.ws_lock:
             if self.ws_state == WsState.OPEN:
                 self.ws_state = WsState.CLOSING
-                task = asyncio.create_task(self.ws.close())
-                task.add_done_callback(
-                    lambda _: logger.debug("old websocket %s closed.", self.ws.id)
-                )
+
+                # Here we schedule the closing of the WebSocket into a background task,
+                # because it can take an arbitrarily long time to perform (since it
+                # waits for the close message). This is normally fast, but in the face
+                # of network disconnects, it can take a couple of minutes for the
+                # kernel to realize that the packets are being lost, and we want to
+                # avoid blocking the reconnect for that to happen.
+                async def _close_ws(ws: WebSocketCommonProtocol) -> None:
+                    try:
+                        await ws.close()
+                    finally:
+                        logger.debug("old websocket %s closed.", ws.id)
+
+                tm.create_task(_close_ws(self.ws))
                 self.ws_state = WsState.CLOSED
