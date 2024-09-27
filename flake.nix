8be28c1687de1865db2cd083e71f8e86964e4f47
lhchavez <lhchavez@lhchavez.com>
2024-09-27 15:55:41 +0000

    [p_unification] I, for one, welcome our new nix overlords
    This repository hadn't been fully nixified.
    
    This repository is now fully nixified.

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "A flake that provides tools needed to hack on river-python";
+
+  inputs.nixpkgs.url = "github:nixos/nixpkgs";
+
+  outputs = { self, nixpkgs }: let
+    mkPkgs = system: import nixpkgs {
+      inherit system;
+    };
+    mkDevShell = system:
+    let
+      pkgs = mkPkgs system;
+      replitNixDeps = (import ./replit.nix { pkgs = pkgs; }).deps;
+    in
+    pkgs.mkShell {
+      env = {
+        # Needed for Python/gRPC to be able to interact with libstdc++.
+        LD_LIBRARY_PATH = "${pkgs.stdenv.cc.cc.lib}/lib";
+      };
+      packages = replitNixDeps;
+    };
+  in
+  {
+    devShells.aarch64-darwin.default = mkDevShell "aarch64-darwin";
+    devShells.x86_64-linux.default = mkDevShell "x86_64-linux";
+  };
+}
