9fefb779c2273a9a88336126cf3fcc0b675cb35f
Devon Stewart <devon.stewart@repl.it>
2024-09-29 22:17:31 -0700

    Exit on error, cleanup after execution

--- scripts/parity.sh
+++ scripts/parity.sh
@@ -3,6 +3,8 @@
 # parity.sh: Generate Pydantic and TypedDict models and check for deep equality.
 #            This script expects that ai-infra is cloned alongside river-python.
 
+set -e
+
 scripts="$(dirname "$0")"
 cd "${scripts}/.."
 
@@ -11,15 +13,23 @@ mkdir "$root/src"
 
 echo "Using $root" >&2
 
+function cleanup {
+  if [ -z "${DEBUG}" ]; then
+    echo "Cleaning up..." >&2
+    rm -rfv "${root}" >&2
+  fi
+}
+trap "cleanup" 0 2 3 15
+
 gen() {
-        fname="$1"; shift
-        name="$1"; shift
-        poetry run python -m replit_river.codegen \
-                client \
-                --output "${root}/src/${fname}" \
-                --client-name "${name}" \
-                ../ai-infra/pkgs/pid2_client/src/schema/schema.json \
-                "$@"
+  fname="$1"; shift
+  name="$1"; shift
+  poetry run python -m replit_river.codegen \
+    client \
+    --output "${root}/src/${fname}" \
+    --client-name "${name}" \
+    ../ai-infra/pkgs/pid2_client/src/schema/schema.json \
+    "$@"
 }
 
 gen tyd.py Pid2TypedDict --typed-dict-inputs
