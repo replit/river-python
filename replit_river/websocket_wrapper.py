1c344d979805a0cbe306a959ab889a17ec2dde61
Ryan Mulligan <ryan@ryantm.com>
2024-10-11 14:13:02 -0700

    [websocket] use background tasks instead of cleanup function
    Why
    ===
    * The cleanup function added a burden on the caller to make sure the
    tasks were awaited.
    * Instead, follow the advice of
    https://docs.python.org/3/library/asyncio-task.html#asyncio.create_task
    and store them in the background properly
    
    What changed
    ===
    * Revert cleanup commit
    * Add background websocket task storage
    
    Test plan
    ===
    * Ran a pytest, and didn't see "Pending task destroyed" exceptions

--- replit_river/websocket_wrapper.py
+++ replit_river/websocket_wrapper.py
@@ -5,6 +5,7 @@ import logging
 from websockets import WebSocketCommonProtocol
 
 logger = logging.getLogger(__name__)
+_background_tasks: set[asyncio.Task] = set()
 
 
 class WsState(enum.Enum):
@@ -29,7 +30,6 @@ class WebsocketWrapper:
             if self.ws_state == WsState.OPEN:
                 self.ws_state = WsState.CLOSING
                 task = asyncio.create_task(self.ws.close())
-                task.add_done_callback(
-                    lambda _: logger.debug("old websocket %s closed.", self.ws.id)
-                )
+                _background_tasks.add(task)
+                task.add_done_callback(_background_tasks.discard)
                 self.ws_state = WsState.CLOSED
