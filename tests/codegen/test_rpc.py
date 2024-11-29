a75534b53b9fdddb8558680d5c276a41202fae95
Devon Stewart <devon.stewart@repl.it>
2024-11-29 11:48:19 -0800

    Only codegen for tests once per pytest invocation

--- tests/codegen/test_rpc.py
+++ tests/codegen/test_rpc.py
@@ -3,14 +3,16 @@ from datetime import timedelta
 
 import pytest
 
+import importlib
 from replit_river.client import Client
 from replit_river.codegen.client import schema_to_river_client_codegen
 from tests.common_handlers import basic_rpc_method
 
 
-@pytest.mark.asyncio
-@pytest.mark.parametrize("handlers", [{**basic_rpc_method}])
-async def test_basic_rpc(client: Client) -> None:
+@pytest.fixture(scope="session", autouse=True)
+def generate_rpc_client() -> None:
+    import tests.codegen.rpc.generated
+
     shutil.rmtree("tests/codegen/rpc/generated")
     schema_to_river_client_codegen(
         "tests/codegen/rpc/schema.json",
@@ -18,6 +20,12 @@ async def test_basic_rpc(client: Client) -> None:
         "RpcClient",
         True,
     )
+    importlib.reload(tests.codegen.rpc.generated)
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize("handlers", [{**basic_rpc_method}])
+async def test_basic_rpc(client: Client) -> None:
     from tests.codegen.rpc.generated import RpcClient
 
     res = await RpcClient(client).test_service.rpc_method(
