0139bbe38bda944f506840e8dd0a9717148c3d83
Devon Stewart <devon.stewart@repl.it>
2025-03-26 09:49:58 -0700

    Type error instead of runtime error

--- src/replit_river/codegen/client.py
+++ src/replit_river/codegen/client.py
@@ -988,7 +988,7 @@ def render_library_call(
                 ]
             )
         else:
-            raise ValueError("Precondition failed")
+            assert_never(protocol_version)
     elif procedure.type == "stream":
         assert output_meta
         assert error_meta
@@ -1084,7 +1084,7 @@ def render_library_call(
                 ]
             )
         else:
-            raise ValueError("Precondition failed")
+            assert_never(protocol_version)
 
     current_chunks.append("")
     return current_chunks
