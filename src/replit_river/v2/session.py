64706256ab9767e1505d5d2c326d155702c83be3
Devon Stewart <devon.stewart@repl.it>
2025-04-15 15:25:23 -0700

    More logging

--- src/replit_river/v2/session.py
+++ src/replit_river/v2/session.py
@@ -293,11 +293,11 @@ class Session[HandshakeMetadata]:
             # This is safe because each individual function that is waiting on this
             # function completeing already has a reference, so we'll last a few ticks
             # before GC.
-            #
-            # Let's do our best to avoid clobbering other tasks by comparing the .name
             current_task = asyncio.current_task()
             if self._connecting_task is current_task:
                 self._connecting_task = None
+            else:
+                logger.debug("unbind_connecting_task failed, id did not match")
 
         if not self._connecting_task:
             self._connecting_task = asyncio.create_task(
@@ -1150,21 +1150,23 @@ async def _do_ensure_connected[HandshakeMetadata](
 
                 raise err
 
+            logger.debug("Connected")
             # We did it! We're connected!
             last_error = None
             rate_limiter.start_restoring_budget(client_id)
             transition_connected(ws)
             break
         except Exception as e:
-            if ws:
-                close_ws_in_background(ws)
-                ws = None
-            last_error = e
             backoff_time = rate_limiter.get_backoff_ms(client_id)
             logger.exception(
                 f"Error connecting, retrying with {backoff_time}ms backoff"
             )
+            if ws:
+                close_ws_in_background(ws)
+                ws = None
+            last_error = e
             await asyncio.sleep(backoff_time / 1000)
+        logger.debug("Here, about to retry")
     unbind_connecting_task()
 
     if last_error is not None:
